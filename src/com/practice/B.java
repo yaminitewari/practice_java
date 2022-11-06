2f6385b8c4433583bcd0359db6f8504cc1deca24
yaminitewari <30841328+yaminitewari@users.noreply.github.com>
2022-11-06 19:53:01 +0530

    comment added

--- src/com/practice/B.java
+++ src/com/practice/B.java
@@ -4,10 +4,13 @@ import com.practice.A;
 
 public class B extends A{
     int num2;
+    int num3;
 
     public B(int a) {
        super(a);
         System.out.print("Constructor of B");
+          System.out.print("Constructor of B");
+          System.out.print("Constructor of B");
     }
 //    public B(int a,int r) {
 //        System.out.print("Constructor of BBBBBBBBBBB");
